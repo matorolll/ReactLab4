bd8e97f5d9e51ef837d01ebef4991410e48df80e
matorolll <mgajos00@gmail.com>
2023-01-30 09:37:08 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [wykladowcy, setWykladowca] = useState([]);
+  const [wybranyWykladowca, setWybranyWykladowca] = useState([]);
+  const [tekst, setTekst] = useState('');
+
+  useEffect(() => { fetch('https://mocki.io/v1/7604b5c4-1df8-4c8e-9fe8-375aa1591eae').then(res => res.json()).then(data => setWykladowca(data.list))}, []);
+
+  const handleClick = wykladowca => setWybranyWykladowca(wykladowca);
+  const handleChange = event => {
+    setTekst(event.target.value);
+    setWybranyWykladowca([]);
+    if (wykladowcy.some(wykladowca => wykladowca.teacher.toLowerCase().includes(tekst.toLowerCase())) === 0) setWybranyWykladowca({});
+  };
+
+  const filteredTeachers = wykladowcy.filter(wykladowca => {
+    return (
+      wykladowca.teacher.toLowerCase().includes(tekst.toLowerCase()) 
+      ||
+      wykladowca.subjects.some(subject => subject.toLowerCase().includes(tekst.toLowerCase()))
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={tekst} onChange={handleChange} />
+      <div> {filteredTeachers.map(wykladowca => ( <div key={wykladowca.id}>  <button onClick={() => handleClick(wykladowca)}> {wykladowca.teacher} </button> </div> ))} </div>
+      {wybranyWykladowca.subjects && ( <ul> {wybranyWykladowca.subjects.map(subject => ( <li key={subject}>{subject}</li> ))} </ul> )}
     </div>
   );
 }
-
 export default App;
